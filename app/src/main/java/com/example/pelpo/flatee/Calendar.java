bbd005d48246cffefe240c81a75f170b3e28ca84
pelpose <pelpose@gmail.com>
2017-06-11 16:24:20 +1200

    added Calendar class (not functions)

--- app/src/main/java/com/example/pelpo/flatee/Calendar.java
+++ app/src/main/java/com/example/pelpo/flatee/Calendar.java
@@ -1,27 +1,44 @@
 package com.example.pelpo.flatee;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
 import android.widget.Toast;
 
 public class Calendar extends AppCompatActivity {
 
     private String roomNum;
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        setRoomNum();
+        showCalender();
+    }
+
+    private void showCalender() {
+        calendarView =(CalendarView) findViewById(R.id.calendar);
+        calendarView.setShowWeekNumber(false);
+        calendarView.setFirstDayOfWeek(2);
+        calendarView.setBackgroundColor(getResources().getColor(R.color.green));
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(),dayOfMonth+"/"+month+"/"+year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menuforcalendar, menu);
-        setRoomNum();
         return true;
     }
 
